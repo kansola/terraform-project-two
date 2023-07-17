dc157d49ecfdaf733cb69eebc763f0413d27b908
Oyinkansola Aremu <oa21adb@herts.ac.uk>
2023-07-17 21:36:33 +0100

    commit

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Set the variable value in *.tfvars file
+# or using -var="do_token=..." CLI option
+variable "do_token" {}
+variable "pub_key" {}
+variable "pvt_key" {}
+variable "ssh_fingerprint" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
---
+++ web1.tf
@@ -0,0 +1,25 @@
+resource "digitalocean_droplet" "www-1" {
+  image = "ubuntu-18-04-x64"
+  name = "www-1"
+  region = "lon1"
+  size = "s-1vcpu-1gb"
+  ssh_keys = [
+    "${var.ssh_fingerprint}"
+  ]
+  
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      # install nginx
+      "sudo apt update",
+      "sudo apt install -y nginx"
+    ]
+  }
+}
