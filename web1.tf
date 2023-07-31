bb83f5faabfd02485f901fca7869da1958542a74
Oyinkansola Aremu <oa21adb@herts.ac.uk>
2023-07-31 02:53:58 +0100

    commit

--- web1.tf
+++ web1.tf
@@ -3,6 +3,7 @@ resource "digitalocean_droplet" "www-1" {
   name = "www-1"
   region = "lon1"
   size = "s-1vcpu-1gb"
+  ipv4_address = "68.183.254.49"
   ssh_keys = [
     "${var.ssh_fingerprint}"
   ]
@@ -18,8 +19,27 @@ resource "digitalocean_droplet" "www-1" {
   provisioner "remote-exec" {
     inline = [
       # install nginx
-      "sudo apt update",
-      "sudo apt install -y nginx"
+      "sudo apt-get update -y",
+      "sudo apt-get install -qy git",
+      "sudo apt-get install -qy openssh-server",
+      "sudo sed -i 's|session    required     pam_loginuid.so|session    optional     pam_loginuid.so|g' /etc/pam.d/sshd",
+      "sudo mkdir -p /var/run/sshd",
+      "sudo apt-get install -qy default-jdk",
+      "sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "sudo apt-key fingerprint 0EBFCD88",
+      "sudo apt-get update -y",
+      "sudo apt-get install docker-ce docker-ce-cli containerd.io -y",
+      "sudo adduser --quiet jenkins",
+      "echo \"jenkins:password\" | chpasswd",
+      "mkdir /home/jenkins/",
+      "COPY .ssh/authorized_keys /home/jenkins/.ssh/authorized_keys",
+      "sudo chmod 700 ~/.ssh",
+      "chmod 600 ~/.ssh/authorized_keys"
+
+
+
     ]
   }
 }
